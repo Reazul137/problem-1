e64c192978a7c1fc2da019f0e6641227916b18d5
Reazul Islam <reazulislam137@gmail.com>
2018-03-04 22:41:48 +0600

    first commit

---
+++ march 4-1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+     printf("Name          : Md. Reazul Islam \n");
+     printf("Date of Birth : 9th Feb 1997 \n");
+     printf("Mobile Number : 017********\n");
+     return 0;
+}
